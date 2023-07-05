44aac79e712234e96ae05b164e72f08b6d49e3d9
Alane-Kerolaine <alanekerolaine@live.com>
2023-07-04 22:13:45 -0300

    primeiro commit adicionando o projeto Spring Boot

---
+++ src/main/java/com/schooljava/mjvschooljobby/MjvSchoolJobbyApplication.java
@@ -0,0 +1,13 @@
+package com.schooljava.mjvschooljobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjvSchoolJobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjvSchoolJobbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schooljava/mjvschooljobby/MjvSchoolJobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schooljava.mjvschooljobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjvSchoolJobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
