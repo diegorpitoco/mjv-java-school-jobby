132a9cbc7950ad9c5f8ba2c896700e13b6b76b36
Estefania Carrer <estefania.carrer@hotmail.com>
2023-07-14 00:40:16 -0300

    Alterações nas classes do model

--- src/main/java/com/schooljava/mjvschooljobby/model/Contato.java
+++ src/main/java/com/schooljava/mjvschooljobby/model/Contato.java
@@ -1,17 +1,30 @@
 package com.schooljava.mjvschooljobby.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
-@Entity
 @Data
+@Entity
+@Table(name = "contato")
 public class Contato {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    @Column(name = "id_contato")
     private Integer idContato;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "telefone")
     private Long telefone;
+
+    @Column(name = "celular")
     private Long celular;
+
+    @Column(name = "whatsapp")
     private boolean whatsapp;
 }
