8cb3e86b9a554c673d0180da7203740a084c688a
Emerson Goncalvez Luiz <emerson_egl@hotmail.com>
2023-07-13 21:02:51 -0300

    Adicionando Camada DTO Inicial

---
+++ src/main/java/com/schooljava/mjvschooljobby/dto/CandidatoDto.java
@@ -0,0 +1,21 @@
+package com.schooljava.mjvschooljobby.dto;
+
+import com.schooljava.mjvschooljobby.enums.Sexo;
+import com.schooljava.mjvschooljobby.model.*;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+public class CandidatoDto {
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private Sexo sexo;
+    private Contato contato;
+    private Endereco endereco;
+    private Profissao profissao;
+    private Experiencia experiencia;
+    private PretensaoSalarial pretensaoSalarial;
+}
--- src/main/java/com/schooljava/mjvschooljobby/service/CandidatoService.java
+++ src/main/java/com/schooljava/mjvschooljobby/service/CandidatoService.java
@@ -1,13 +1,26 @@
 package com.schooljava.mjvschooljobby.service;
 
+import com.schooljava.mjvschooljobby.dto.CandidatoDto;
 import com.schooljava.mjvschooljobby.repository.CandidatoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.util.BeanDefinitionUtils;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CandidatoService {
 
-    final CandidatoRepository cadastroRepository;
+    //final CandidatoRepository cadastroRepository;
+    @Autowired
+    private  CandidadoRepository repository;
+//    public void gravar(CandidatoDto dto) {
+//        CandidatoEntity entity = new CandidatoEntity();
+//        BeanUtils.copyProperties(dto, entity);
+//        repository.save(entity);
+
+    }
 
 }
