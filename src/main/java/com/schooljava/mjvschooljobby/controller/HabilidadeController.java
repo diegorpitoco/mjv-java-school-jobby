efa7756b6ce08a4470299978377a99c8d0d5471a
Estefania Carrer <estefania.carrer@hotmail.com>
2023-07-15 13:03:52 -0300

    Atualizando classes controller

--- src/main/java/com/schooljava/mjvschooljobby/controller/HabilidadeController.java
+++ src/main/java/com/schooljava/mjvschooljobby/controller/HabilidadeController.java
@@ -1,5 +1,6 @@
 package com.schooljava.mjvschooljobby.controller;
 
+import com.schooljava.mjvschooljobby.service.HabilidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/schooljava/mjvschooljobby/controller/ProfissaoController.java
+++ src/main/java/com/schooljava/mjvschooljobby/controller/ProfissaoController.java
@@ -1,11 +1,12 @@
 package com.schooljava.mjvschooljobby.controller;
 
+import com.schooljava.mjvschooljobby.service.ProfissaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/profissao")
+@RequestMapping("/profissão")
 public class ProfissaoController {
 
     @Autowired
