9fe833ae6ea11e6f650b4188eb7cae756f8146ef
Ismael Miranda <ismael@ismael.com>
2023-07-15 12:33:47 -0300

    Criado as classes da service

---
+++ src/main/java/com/schooljava/mjvschooljobby/service/CidadeService.java
@@ -0,0 +1,12 @@
+package com.schooljava.mjvschooljobby.service;
+
+import com.schooljava.mjvschooljobby.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CidadeService {
+
+    @Autowired
+    CidadeRepository cidadeRepository;
+}
